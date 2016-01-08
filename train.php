e79e2cf2e650fee137894811a11a2437b7395b21
windform <867185661@qq.com>
2016-01-08 16:00:01 +0800

    Update train.php
    完善ajax查询。既可以通过车次查询也可以通过站站查询。
    思路：
    1、初始化，模拟点击单选按钮1，默认显示的是“列次查询”；
    2、点击单选按钮时，通过判断checked属性来决定是哪些查询条件显示；
    3、设置变量address，当submit点击时，通过判断哪个单选按钮被选中，从而将需要发送ajax请求的url赋值给address；
    4、展现数据时，设置变量pan来保存布尔值。通过address里面的特有字段“query_ab”用正则表达式的test()方法验证真假，把验证的结果保存在pan；

--- train.php
+++ train.php
@@ -85,34 +85,60 @@
 
     <script src="http://cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
     <script src="http://cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-	<script>
+     <script>
 		$(function(){
-			$(".dis").css("display","none");
+			//$(".dis").css("display","none");
+			$("#inlineCheckbox1").click(function(){
+						$(".dis1").css("display","block");
+						$(".dis").css("display","none");
+						$("#from").val("");
+						$("#to").val("");
+				});
+			//初始化，模拟点击#inlineCheckbox1
+			$("#inlineCheckbox1").trigger('click');
 			$("input[type='radio']").click(function(){
 				if($("#inlineCheckbox1").is(":checked")){
 						$(".dis1").css("display","block");
 						$(".dis").css("display","none");
 						$("#from").val("");
 						$("#to").val("");
+						//address='http://op.juhe.cn/onebox/train/query?train='+$("#train").val()+'&key='+$("#key").val();
 					}else if($("#inlineCheckbox2").is(":checked")){
 						$(".dis1").css("display","none");
 						$(".dis").css("display","block");
 						$("#train").val("");
+						//address='http://op.juhe.cn/onebox/train/query_ab?train='+$("#train").val()+'&from='+$("#from").val()+'&to='+$("#to").val()+'&key='+$("#key").val();
 						}
 				})
 			$("#submit").click(function(){
+				
+					var address;
+					if($("#inlineCheckbox1").is(":checked")){
+						address='http://op.juhe.cn/onebox/train/query?train='+$("#train").val()+'&key='+$("#key").val();
+					}else if($("#inlineCheckbox2").is(":checked")){
+						address='http://op.juhe.cn/onebox/train/query_ab?from='+$("#from").val()+'&to='+$("#to").val()+'&key='+$("#key").val();
+						}
+					//定义一个变量，根据radio的checked属性来给变量address赋值，接在把它赋值给ajax的url
+				
 				$.ajax({
-					//http://op.juhe.cn/onebox/train/query_ab?train='+$("#train").val()+'&key='+$("#key").val(),
-					url:'http://op.juhe.cn/onebox/train/query_ab?train='+$("#train").val()+'&from='+$("#from").val()+'&to='+$("#to").val()+'&key='+$("#key").val(),
+					url:address,
 					type:'get',
+					timeout:5000,
 					dataType:'JSONP',
 					beforeSend: function(){
 						$("#result").html("<div class='mask'><img width='50' height='50' src='loading.gif' alt='加载查询'/></div>")
 						
 						},
 					success:function(result){
+						//var ad='http://op.juhe.cn/onebox/train/query?train='+$("#train").val()+'&key='+$("#key").val();
+						//alert(address);
 						var arr=result.result.list;
 						var data='<table class="table table-bordered table-striped table-hover">'+'<thead>'+'<tr>'+'<th>车次</th>'+'<th>类型</th>'+'<th>始发站</th>'+'<th>终点站</th>'+' <th>出发时间</th>'+' <th>到达时间</th>'+'<th>时长</th>'+'</tr>'+'</thead>'+'<tbody>';
+						
+						var pattern=/query_ab/i;
+						var pan=pattern.test(address);
+						//根据车次查询的特有字段'auery_ab'来匹配政策表达式，如果包含，返回true；否则返回false；pan=true时，展示的站到站的查询数据；否则展示的是列次查询的数据
+						if(pan==true){
 							for(var i=0;i<arr.length;i++){
 								data+='<tr>';
 								for(j in arr[i]){
@@ -122,12 +148,33 @@
 								data+='</tr>';
 									
 								}
+							
+						}else{
+								data+='<tr>'
+									+'<td>'+arr['train_no']+'</td>'
+									+'<td>'+arr['train_type']+'</td>'
+									+'<td>'+arr['start_station']+'</td>'
+									+'<td>'+arr['end_station']+'</td>'
+									+'<td>'+arr['start_time']+'</td>'
+									+'<td>'+arr['end_time']+'</td>'
+									+'<td>'+arr['run_time']+'</td>'
+								'</tr>';
+							}
+								
+		
 							data+='</tbody>';
 							data+='</table>';	
 						$("#result").html(data);
 						},
 					error:function(){
 						$("#result").html("对不起,请求失败啦");
+						},
+					complete:function(XMLHttpRequest,status){
+						if(status=='timeout'){
+							//ajaxTimeoutTest.absort();
+							$("#result").html("查询失败");
+							}
+						
 						}
 					})
 				return false;
